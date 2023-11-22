7525051161fd6eb4bfc29a3447583150ec662f06
AshMa1022 <wma@risd.edu>
2023-11-21 20:54:00 -0500

    init

--- build/build-Realtime_filter-Qt_6_5_2_for_macOS-Debug/CMakeFiles/clean_additional.cmake
+++ build/build-Realtime_filter-Qt_6_5_2_for_macOS-Debug/CMakeFiles/clean_additional.cmake
@@ -5,9 +5,9 @@ if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
   file(REMOVE_RECURSE
   "CMakeFiles/StaticGLEW_autogen.dir/AutogenUsed.txt"
   "CMakeFiles/StaticGLEW_autogen.dir/ParseCache.txt"
-  "CMakeFiles/projects_realtime_autogen.dir/AutogenUsed.txt"
-  "CMakeFiles/projects_realtime_autogen.dir/ParseCache.txt"
+  "CMakeFiles/realtime_filter_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/realtime_filter_autogen.dir/ParseCache.txt"
   "StaticGLEW_autogen"
-  "projects_realtime_autogen"
+  "realtime_filter_autogen"
   )
 endif()
